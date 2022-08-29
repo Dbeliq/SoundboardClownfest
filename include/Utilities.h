7a661e47cea26e64c8aaebd21aaa0feef854395b
Dbeliq <94119537+Dbeliq@users.noreply.github.com>
2022-08-29 13:54:43 +0300

    utilities header and main

---
+++ include/Utilities.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <string>
+#include <algorithm>
+#include <iterator>
+#include <mmsystem.h>
+
+#include "Path.h"
+#include "WavHeader.h"
+
+/* Helper function for finding if a list contains an element */
+
+template <typename T>
+bool ListContains(std::list<T> & listOfElements, const T & element)
+{
+    // Find the iterator if element in list
+    auto it = std::find(listOfElements.begin(), listOfElements.end(), element);
+    // return if iterator points to end or not. It points to end then it means element
+    // does not exists in list
+    return it != listOfElements.end();
+};
+
+/* Helper function to transform WavHeader to WFX so i can  */
+
+WAVEFORMATEX WavHeaderToWfx(WavHeader wavHeader) {
+    WAVEFORMATEX wfx;
+    wfx.nSamplesPerSec = wavHeader.sample_rate;
+    wfx.wBitsPerSample = wavHeader.bits_per_sample;
+    wfx.nChannels = wavHeader.num_channels;
+    
+    wfx.cbSize = 0;
+    wfx.wFormatTag = WAVE_FORMAT_PCM;
+    wfx.nBlockAlign = wavHeader.block_align;
+    wfx.nAvgBytesPerSec = wavHeader.byte_rate;
+
+    return wfx;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,22 @@
 #include <iostream>
-
-
-#include <iostream>
 #include <windows.h>
 #include <mmsystem.h>
 #include <stdio.h>
 #include <string>
 #include <thread>
 #include <list>
-#include <algorithm>
-#include <iterator>
 
-#include "Path.h"
+
+#include "Utilities.h"
 #include "AudioFileHandler.h"
+#include "WavHeader.h"
 
 static std::list<char> pressedKeys;
 static AudioFileHandler audioFileHandler;    
 static LPSTR block;
 static DWORD blockSize;
 
-/* Helper function for finding if a list contains an element */
-/* Maybe add it to a utils class? */
 
-template <typename T>
-bool contains(std::list<T> & listOfElements, const T & element)
-{
-    // Find the iterator if element in list
-    auto it = std::find(listOfElements.begin(), listOfElements.end(), element);
-    // return if iterator points to end or not. It points to end then it means element
-    // does not exists in list
-    return it != listOfElements.end();
-}
 
 /* The keyboard hook. It's low level so we can't get the last key state */
 /* So we just need to keep track of what keys were pressed and check them before we handle the press */
@@ -43,7 +29,7 @@ LRESULT CALLBACK KBDHOOK(int nCode, WPARAM wParam, LPARAM lParam) {
         case WM_KEYDOWN: {
             char c = MapVirtualKey(s->vkCode, MAPVK_VK_TO_CHAR);
             
-            if(!contains(pressedKeys, c)) {
+            if(!ListContains(pressedKeys, c)) {
                 pressedKeys.push_back(c);
                 if(s->vkCode == 0x41) {
                     std::thread th(&AudioFileHandler::PlayRawFile, audioFileHandler, "E:\\Random\\Projects\\C++\\Homofold\\E\\test.raw", 1);
@@ -54,7 +40,7 @@ LRESULT CALLBACK KBDHOOK(int nCode, WPARAM wParam, LPARAM lParam) {
                     th.detach();
                 }
                 if(c == 'D') {
-                    std::thread th(&AudioFileHandler::PlayBlock, audioFileHandler, block, blockSize, 1);
+                    std::thread th(&AudioFileHandler::PlayBlock, audioFileHandler, block, blockSize, audioFileHandler.GetDefaultWfx(), 1);
                     th.detach();
                 }
             }      
@@ -74,22 +60,34 @@ int main() {
     
     printf("Main started\n");
 
-    audioFileHandler.GetWaveDevicesInfo();
+    //audioFileHandler.GetWaveDevicesInfo();
 
-   
-    block = audioFileHandler.LoadRawAudioBlock("E:\\Random\\Projects\\C++\\Homofold\\E\\omg.raw", &blockSize);
+    WavHeader wavHeader;
+
+    audioFileHandler.LoadWavFile("gawd.wav", wavHeader);
+
+    WAVEFORMATEX wfx = WavHeaderToWfx(wavHeader);
+    
+
+    audioFileHandler.PlayBlock((LPSTR)wavHeader.data, wavHeader.data_bytes, wfx, 1);
 
 
-    HHOOK kbd = SetWindowsHookEx(WH_KEYBOARD_LL, &KBDHOOK, 0, 0);
 
-    MSG message;
-    while(GetMessage(&message, NULL, 0, 0) > 0) {
-        TranslateMessage(&message);
-        DispatchMessage(&message);
-    }
 
-    UnhookWindowsHookEx(kbd);
 
-    system("pause");
+
+    //block = audioFileHandler.LoadRawAudioBlock("E:\\Random\\Projects\\C++\\Homofold\\E\\omg.raw", &blockSize);
+
+
+    // HHOOK kbd = SetWindowsHookEx(WH_KEYBOARD_LL, &KBDHOOK, 0, 0);
+
+    // MSG message;
+    // while(GetMessage(&message, NULL, 0, 0) > 0) {
+    //     TranslateMessage(&message);
+    //     DispatchMessage(&message);
+    // }
+
+    // UnhookWindowsHookEx(kbd);
+
     return 0;
 }
